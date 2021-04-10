522913bfad33ee135ee5afcbda87c113655f21bb
KamroozNaini <Kamroozi_gh@yahoo.com>
2021-04-10 17:33:01 -0400

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\hrmanagementbot-Web-Deploy.pubxml -p:Password=keopCBjy2MdhCFiCqYomBD1g50xG3KJxinhjtsYFwyi8usqjwHn0oqgjDDiX
+
